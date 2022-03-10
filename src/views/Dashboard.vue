ba8f53627ec27d6c760abf8452787bed6cea1db4
ruslansobol <ruslansobol090@protonmail.com>
2022-03-09 19:44:19 -0800

    use ssh-action

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,30 +1,31 @@
 <template>
-    <div>
-        <v-container fluid>
-            <v-row class="pa-md-4 mx-lg-auto" style="margin-top:30px">
-                    <h3>People you follow</h3>
-            </v-row>
-            <v-card v-if="followers.length!=0">
-                <span v-for="follower in followers" :key="follower.id">
-                <router-link
-                        :to="{ name: 'user', params: { userId: follower.id } }"
-                    >
-                    <v-avatar 
-                        color="orange"
-                        size="70"
-                        style="margin:7px" 
-                    >                 
-                        <v-img  :src="follower.photo_url"/>    
-                    </v-avatar>
-                </router-link>
-                </span>
-            </v-card>
-            <v-container v-else>
-                &nbsp;&nbsp; No following people yet
-            </v-container>
-            <StoryList />
+<div>
+    <v-container fluid>
+        <v-row class="pa-md-4 mx-lg-auto" style="margin-top:30px">
+                <h3>People you follow</h3>
+        </v-row>
+        <v-card v-if="followers.length!=0">
+            <span v-for="follower in followers" :key="follower.id">
+            <router-link
+                    :to="{ name: 'user', params: { userId: follower.id } }"
+                >
+                <v-avatar 
+                    color="orange"
+                    size="70"
+                    style="margin:7px" 
+                >                 
+                    <v-img  :src="follower.photo_url"/>    
+                </v-avatar>
+            </router-link>
+            </span>
+        </v-card>
+        <v-container v-else>
+            &nbsp;&nbsp; No following people yet
         </v-container>
-    </div>
+        <StoryList />
+    </v-container>
+    
+</div>
 </template>
 
 <script>
