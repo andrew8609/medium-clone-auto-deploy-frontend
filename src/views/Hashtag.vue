4e753f4417d578c9276b945427de8254c3a49425
ruslansobol <ruslansobol090@protonmail.com>
2022-03-10 05:35:17 -0800

    use ssh-action

--- src/views/Hashtag.vue
+++ src/views/Hashtag.vue
@@ -12,31 +12,31 @@
         line-height: 1.3rem;
     };
 </style>
-<template>
-    <v-container>
-        <div style="margin:40px">            
-        <h2><b>#</b>{{hashtagStories.hashtag_name}}</h2>
-        </div>
-        <v-divider horizontal />
-        <div v-for="(story, index) of hashtagStories.stories" :key=index>
-            <router-link :to="viewStoryLink(story.Story.id)" class="preview-link">
-                <v-row >
-                    <v-col cols="9">
-                        <h6>{{story.Story.created_at}}</h6>
-                        <h4><b>{{story.Story.title}}</b></h4>
-                        <div class="brief-content" v-if="story.Story.content_pure">{{briefStory(story.Story.content_pure)}}</div>
-                    </v-col>
-                    <v-col cols="3" align="center"
-                        justify="center">
-                        <div v-if="getImage(story.Story.content)" v-html="getImage(story.Story.content)">
-                        </div>
-                        <img v-else src="https://www.seekpng.com/png/detail/73-730482_existing-user-default-avatar.png"/>       
-                    </v-col>
-                </v-row>
-            </router-link>      
+    <template>
+        <v-container>
+            <div style="margin:40px">            
+            <h2><b>#</b>{{hashtagStories.hashtag_name}}</h2>
+            </div>
             <v-divider horizontal />
-        </div>
-    </v-container>
+            <div v-for="(story, index) of hashtagStories.stories" :key=index>
+                <router-link :to="viewStoryLink(story.Story.id)" class="preview-link">
+                    <v-row >
+                        <v-col cols="9">
+                            <h6>{{story.Story.created_at}}</h6>
+                            <h4><b>{{story.Story.title}}</b></h4>
+                            <div class="brief-content" v-if="story.Story.content_pure">{{briefStory(story.Story.content_pure)}}</div>
+                        </v-col>
+                        <v-col cols="3" align="center"
+                            justify="center">
+                            <div v-if="getImage(story.Story.content)" v-html="getImage(story.Story.content)">
+                            </div>
+                            <img v-else src="https://www.seekpng.com/png/detail/73-730482_existing-user-default-avatar.png"/>       
+                        </v-col>
+                    </v-row>
+                </router-link>      
+                <v-divider horizontal />
+            </div>
+        </v-container>
 </template>
 <script>
 
